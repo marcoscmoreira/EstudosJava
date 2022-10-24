59882a04a3e60819ebc6cfb55cc15a1179e5e2e6
Marcos <Marcos@Marcos-Note.home>
2022-10-24 23:20:27 +0100

    Criação do primeiro

---
+++ eclipse-workspace/primeiro-projeto-java/src/cursojava/executavel/PrimeriaClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeriaClasseJava {
+	
+	/* Main é um método auto executavel em java */
+	public static void main(String[] args) {
+		/*Escreve texto no console*/
+		System.out.println("Hello World");
+	}
+	
+
+}
