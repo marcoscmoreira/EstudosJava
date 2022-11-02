0441a5480ae2fa1f8dc41e3859c7fd0f4e46667c
Marcos <Marcos@Marcos-Note.home>
2022-11-02 00:11:09 +0000

    Listas + FOR

--- eclipse-workspace/primeiro-projeto-java/src/cursojava/classes/Aluno.java
+++ eclipse-workspace/primeiro-projeto-java/src/cursojava/classes/Aluno.java
@@ -1,5 +1,7 @@
 package cursojava.classes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Aluno {
@@ -15,10 +17,17 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	
 
 	/* Construtor */
 	public Aluno() { /* Cria dados na memória */
@@ -118,52 +127,18 @@ public class Aluno {
 
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
-	}
+	} 
 
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
 	
 	
-	/* Métodos toString */
+
 	@Override
 	public String toString() {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCps=" + numeroCps + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-				+ "]";
+				+ serieMatriculado + ", disciplinas=" + disciplinas + "]";
 	}
-	
-	
 
 	@Override
 	public int hashCode() {
@@ -184,7 +159,15 @@ public class Aluno {
 
 	/*Método que retorna a média do aluno*/
 	public double getMediaNota () {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		double somaNotas = 0.0;
+		
+		
+		for (Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+		}
+		
+		return somaNotas / disciplinas.size();
 	}
 	
 	/*Métodos que retorna true ou false para validar 
--- eclipse-workspace/primeiro-projeto-java/src/cursojava/executavel/PrimeriaClasseJava.java
+++ eclipse-workspace/primeiro-projeto-java/src/cursojava/executavel/PrimeriaClasseJava.java
@@ -3,43 +3,49 @@ package cursojava.executavel;
 import javax.swing.JOptionPane;
 
 import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
 
 public class PrimeriaClasseJava {
-	
+
 	/* Main é um método auto executavel em java */
 	public static void main(String[] args) {
 
-		
-		/*new Aluno() é uma instancia (Criação de objeto)*/
+		/* new Aluno() é uma instancia (Criação de objeto) */
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno: ");
 		String idade = JOptionPane.showInputDialog("Qual a idade: ");
 		String dataNascimento = JOptionPane.showInputDialog("Data de Nascimento: ");
+
+		Aluno aluno = new Aluno();
+		aluno.setNome(nome);
+		aluno.setIdade(Integer.valueOf(idade)); /* O integer faz o papel de conversão da string que ele recebe */
+		aluno.setDataNascimento(dataNascimento);
+		aluno.setRegistroGeral("654654646465");
+		aluno.setNumeroCps("551515");
+		aluno.setNomeMae("Nome da mãe");
+		aluno.setNomePai("Algun pai ai");
+		aluno.setDataMatricula("15/12/5165");
+		aluno.setSerieMatriculado("6");
+		aluno.setNomeEscola("Java Java");
 		
-		
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade)); /*O integer faz o papel de conversão da string que ele recebe*/
-		aluno1.setDataNascimento(dataNascimento);
-		aluno1.setRegistroGeral("654654646465");
-		aluno1.setNumeroCps("551515");
-		aluno1.setNomeMae("Nome da mãe");
-		aluno1.setNomePai("Algun pai ai");
-		aluno1.setDataMatricula("15/12/5165");
-		aluno1.setSerieMatriculado("6");
-		aluno1.setNomeEscola("Java Java");
-		aluno1.setNota1(90);
-		aluno1.setNota2(80.8);
-		aluno1.setNota3(70.9);
-		aluno1.setNota4(90.7);
-		
-		System.out.println("Nome: " + aluno1.getNome());
-		System.out.println("Idade: " + aluno1.getIdade());
-		System.out.println("Nascimento: " + aluno1.getDataNascimento());
-		System.out.println("Média da nota é: " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado")); /*Operador ternario de condição IF*/
-		
-		
+		for (int pos = 1; pos <= 4; pos++ ) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina "+pos+"?");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina "+pos+" ?");
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno.getDisciplinas().add(disciplina);
+			
+		}
+
+		System.out.println("Nome: " + aluno.getNome());
+		System.out.println("Idade: " + aluno.getIdade());
+		System.out.println("Nascimento: " + aluno.getDataNascimento());
+		System.out.println("Média da nota é: " + aluno.getMediaNota());
+		System.out.println("Resultado = "
+				+ (aluno.getAlunoAprovado() ? "Aprovado" : "Reprovado")); /* Operador ternario de condição IF */
+
 	}
-	
 
 }
